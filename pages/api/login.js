3bcb9eebd3d734c49a343896afed824a60cf7b4b
kindled <k1ndled11@gmail.com>
2022-04-29 23:32:38 -0400

    Fix bug in OAuth

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
   res.redirect(
-    "https://discord.com/api/oauth2/authorize?client_id=969790016496746516&redirect_uri=https%3A%2F%2Fserver-counter.vercel.app%2Fcb&response_type=tokenscope=identify%20guilds"
+    "https://discord.com/api/oauth2/authorize?client_id=969790016496746516&redirect_uri=https%3A%2F%2Fserver-counter.vercel.app%2Fcb&response_type=token&scope=identify%20guilds"
   );
 }
