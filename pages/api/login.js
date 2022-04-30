3bc0a472444e359a75da65756eaf6bf4e4caf118
kindled <k1ndled11@gmail.com>
2022-04-29 23:30:56 -0400

    Update OAuth URL

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,3 +1,5 @@
 export default async function handler(req, res) {
-    res.redirect('https://discord.com/api/oauth2/authorize?client_id=969790016496746516&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fcb&response_type=token&scope=identify%20guilds')
+  res.redirect(
+    "https://discord.com/api/oauth2/authorize?client_id=969790016496746516&redirect_uri=https%3A%2F%2Fserver-counter.vercel.app%2Fcb&response_type=tokenscope=identify%20guilds"
+  );
 }
